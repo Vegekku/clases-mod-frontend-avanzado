ecd8f686bee5eecfc74d58dd74716e4677b542b6
Jesús Moyano Ruz <jesus@sopinet.com>
2019-01-29 21:17:14 +0100

    Resuelto carga de css

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './styles/reset.css';
+import './styles/navbar.css';
+import './styles/index.css';
+
+console.log('HELLO WORLD!!!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
-const htmlPlugin = require('html-webpack-plugin');
+const htmlPlugin = require('html-webpack-plugin'); // empaquetador de HTML
 
 const srcDir = 'src';
 
 module.exports = {
     mode: 'development',
-    entry: path.join(__dirname, srcDir, 'js/main.js'), // src/js/main.js
+    entry: path.join(__dirname, srcDir, 'main.js'), // src/js/main.js
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ // se usan de derecha a izquierda, o de abajo a arriba
+                    'style-loader', // procesar el import
+                    'css-loader' // entender el import
+                ]
+            }
+        ],
     },
     plugins: [
         new htmlPlugin({
